e7f992918a6248d7b3ad3f28abe2c962ec1f10d3
poojitha <poojitha.1905l1@gmail.com>
2024-02-14 14:23:44 +0530

    "using dotenv"

---
+++ 05_session5/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT
+
+app.get('/',(req,res)=>{
+    res.send("hello world")
+})
+
+
+app.listen(port,()=>{
+    console.log(`server is running at ${port}`);
+})
